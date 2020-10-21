34b1cbd074d3a3c7cf38aac26d0b26b55614418a
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2020-10-21 20:55:13 +1100

    CAL-11: Added GetHandler method to multiplier package

--- multiplier/multiplier.go
+++ multiplier/multiplier.go
@@ -25,7 +25,11 @@ package multiplier
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"google.golang.org/grpc"
+	"log"
+	"net/http"
 )
 
 func RPCMultiplyFloat(multiplierServiceAddress *string, a, b float32) (float32, error) {
@@ -44,4 +48,41 @@ func RPCMultiplyFloat(multiplierServiceAddress *string, a, b float32) (float32, 
 		return 0.0, err
 	}
 	return res.GetR(), nil
+}
+
+type MultiplyFloatsOperand struct {
+	A float32 `json:"A"`
+	B float32 `json:"B"`
+}
+
+func GetHandler(multiplierServiceAddress *string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("recieved multiply float request")
+		decoder := json.NewDecoder(r.Body)
+
+		var operands MultiplyFloatsOperand
+
+		err := decoder.Decode(&operands)
+		if err != nil {
+			log.Printf("error occured when decoding message: %V", err)
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = fmt.Fprintf(w, "Could not unpack request body: %V", err)
+
+			return
+		}
+
+		log.Printf("recieved operands A=%V , B=%V", operands.A, operands.B)
+
+		res, err := RPCMultiplyFloat(multiplierServiceAddress, operands.A, operands.B)
+		if err != nil {
+			log.Printf("error when remote calling multiply float: %V", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = fmt.Fprintf(w, "Could not multiply values: %V", err)
+
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprint(w, res)
+	}
 }
